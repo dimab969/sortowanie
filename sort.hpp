e3afd071246c56eb5b05d3261efde04de53eb80c
dimab969 <156686650+dimab969@users.noreply.github.com>
2024-02-12 16:34:02 +0100

    Add files via upload

---
+++ sort.hpp
@@ -0,0 +1,10 @@
+#ifndef SORTING_HPP
+#define SORTING_HPP
+
+void bubbleSort(int arr[], int n);
+void cocktailSort(int arr[], int n);
+void mergeSort(int arr[], int l, int r);
+void quickSort(int arr[], int low, int high);
+void insertionSort(int arr[], int n);
+
+#endif 
